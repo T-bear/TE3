44b3bf4ce5d212fae23dc61553cc69b6a1e3301a
Torbjörn Holgersson <torbjorn.holgersson@skola.karlskrona.se>
2020-09-11 09:05:08 +0200

    adding the class test for students

---
+++ testClass/testNonClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace testNonClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string person;
+            person = "Gilgamesh";
+            Console.WriteLine("The heroes is: " + person);
+            int atk;
+            atk = 14;
+            Console.WriteLine("\nwith a total attack power of: " + atk);
+            string person1;
+            person1 = "Bob";
+            Console.WriteLine("\nThe villain is: " + person1);
+            int atk1;
+            atk1 = 13;
+            Console.WriteLine("\nwith a total attack power of: " + atk1);
+        }
+    }
+}
