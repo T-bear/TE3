aab87519209556ea58ba53aeb4963ba3b667573b
Torbjörn Holgersson <torbjorn.holgersson@skola.karlskrona.se>
2020-09-11 09:09:56 +0200

    added the correct folder

---
+++ classTestet/classTestet/Character.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classTestet
+{
+    public class Character
+    {
+        private string name = "";
+        private string title = "";
+        public int atk;
+
+        public Character(string name, string title, int atk)
+        {
+            this.name = name;
+            this.title = title;
+            this.atk = atk;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = Name; }
+        }
+        public string Title
+        {
+            get { return title; }
+            set { title = Title; }
+        }
+        
+        public int Atk
+        {
+            get { return atk; }
+            set { atk = Atk; }
+        }
+
+        public override string ToString()
+        {
+            return name + " " + title;
+        }
+
+
+    }
+}
